99e0f16736145c5cf8c92c5e5df7d53537d7b217
chizycodes <okwudirejoy@gmail.com>
2022-11-05 16:05:54 +0100

    feat: validation

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,27 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const Contact = () => {
 	const [isValid, setIsValid] = useState(false);
+	const [formData, setFormData] = useState({
+		firstName: '',
+		lastName: '',
+		email: '',
+		message: '',
+		accept: false,
+	});
 	let name = 'Joy Okwudire';
+
+	const onChange = (e) => {
+		if (e.target.name === 'accept') {
+			setFormData({ ...formData, [e.target.name]: !formData.accept });
+		} else {
+			setFormData({ ...formData, [e.target.name]: e.target.value });
+		}
+	};
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
+		setFormData({ firstName: '', lastName: '', email: '', message: '', accept: false });
 	};
 	return (
 		<section className="mt-[54px] w-full max-w-[720px] px-3">
@@ -14,7 +30,7 @@ const Contact = () => {
 				Hi there, contact me to ask me about anything you have in mind.
 			</p>
 
-			<form className="w-full">
+			<form className="w-full" onSubmit={handleSubmit}>
 				<div className="grid grid-cols-12 gap-[24px] w-full mb-[24px]">
 					<div className="flex flex-col col-span-12 md:col-span-6">
 						<label className="text-[#344054]" htmlFor="first_name">
@@ -23,8 +39,12 @@ const Contact = () => {
 						<input
 							type="text"
 							id="first_name"
+							name="firstName"
 							placeholder="Enter your first name"
-							className="border border-[#D0D5DD] rounded-[8px] py-[10px] px-[14px] mt-[6px] focus:border-[#84CAFF] outline-none d-shadow placeholder:text-[#667085]"
+							onChange={onChange}
+							value={formData.firstName}
+							required
+							className="border border-[#D0D5DD] rounded-[8px] py-[10px] px-[14px] mt-[6px] focus:valid:border-[#84CAFF] focus:invalid:border-[#F89687] outline-none d-shadow placeholder:text-[#667085]"
 						/>
 					</div>
 
@@ -35,8 +55,12 @@ const Contact = () => {
 						<input
 							type="text"
 							id="last_name"
+							name="lastName"
 							placeholder="Enter your last name"
-							className="border border-[#D0D5DD] rounded-[8px] py-[10px] px-[14px] mt-[6px] focus:border-[#84CAFF] outline-none d-shadow placeholder:text-[#667085]"
+							onChange={onChange}
+							value={formData.lastName}
+							required
+							className="border border-[#D0D5DD] rounded-[8px] py-[10px] px-[14px] mt-[6px] focus:valid:border-[#84CAFF] focus:invalid:border-[#F89687] outline-none d-shadow placeholder:text-[#667085]"
 						/>
 					</div>
 				</div>
@@ -48,8 +72,12 @@ const Contact = () => {
 					<input
 						type="text"
 						id="email"
+						name="email"
 						placeholder="yourname@email.com"
-						className="border border-[#D0D5DD] rounded-[8px] py-[10px] px-[14px] mt-[6px] focus:border-[#84CAFF] outline-none d-shadow placeholder:text-[#667085]"
+						onChange={onChange}
+						value={formData.email}
+						required
+						className="border border-[#D0D5DD] rounded-[8px] py-[10px] px-[14px] mt-[6px] focus:valid:border-[#84CAFF] focus:invalid:border-[#F89687] outline-none d-shadow placeholder:text-[#667085]"
 					/>
 				</div>
 
@@ -60,20 +88,33 @@ const Contact = () => {
 					<textarea
 						type="text"
 						id="message"
+						name="message"
 						placeholder="Send me a message and I'll reply you as soon as possible..."
-						className="border border-[#D0D5DD] rounded-[8px] py-[10px] px-[14px] mt-[6px] focus:border-[#84CAFF] outline-none h-[132px] d-shadow placeholder:text-[#667085]"
+						onChange={onChange}
+						value={formData.message}
+						required
+						className="border border-[#D0D5DD] rounded-[8px] py-[10px] px-[14px] mt-[6px] focus:valid:border-[#84CAFF] focus:invalid:border-[#F89687] outline-none h-[132px] d-shadow placeholder:text-[#667085]"
 					/>
 				</div>
 
 				<div className="flex gap-2 mb-[32px]">
-					<input type="checkbox" className="w-[20px] h-[20px] mt-[2px] d-shadow" />
-					<label className="text-[#475467]">You agree to providing your data to {name} who may contact you.</label>
+					<input
+						type="checkbox"
+						id="accept"
+						name="accept"
+						onChange={onChange}
+						checked={formData.accept}
+						required
+						className="w-[20px] h-[20px] mt-[2px] d-shadow rounded-md border border-[#D0D5DD]"
+					/>
+					<label className="text-[#475467]" htmlFor="accept">
+						You agree to providing your data to {name} who may contact you.
+					</label>
 				</div>
 
 				<button
 					type="submit"
-					onClick={handleSubmit}
-					className="text-white bg-[#1570EF] py-[12px] rounded-[8px] w-full"
+					className="text-white bg-[#1570EF] py-[12px] rounded-[8px] w-full disabled:bg-[#B2DDFF]"
 					id="btn__submit"
 				>
 					Send message
