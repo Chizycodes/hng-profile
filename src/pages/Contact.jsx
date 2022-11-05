d8afe318d21f9cee34eb59fae44d663af49c5cdc
chizycodes <okwudirejoy@gmail.com>
2022-11-05 16:07:06 +0100

    fix: unused values

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,6 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 const Contact = () => {
-	const [isValid, setIsValid] = useState(false);
 	const [formData, setFormData] = useState({
 		firstName: '',
 		lastName: '',
